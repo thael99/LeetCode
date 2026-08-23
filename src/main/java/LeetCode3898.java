158320b6923aad650c952c5e4db3affd4ee41bcc
Thael <thaelzaruz@gmail.com>
2026-08-23 03:50:31 -0300

    Problem 3898 resolution

---
+++ src/main/java/LeetCode3898.java
@@ -0,0 +1,27 @@
+void main() {
+
+    int[][] matriz;
+    matriz = new int[][]{{0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
+    int[] ans;
+    ans = findDegree(matriz);
+    IO.println(Arrays.toString(ans));
+}
+
+static public int[] findDegree(int[][] matrix) {
+    int[] degree;
+    degree = new int[matrix.length];
+    int soma = 0;
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            soma = soma + matrix[i][j];
+        }
+        degree[i] = soma;
+        //System.out.println(degree[i] + " ");
+        soma = 0;
+    }
+
+
+    return degree;
+}
+
+
